12bcf827d6616500b25d787dda50d31315882c4b
vadim <gabderahmanov99@gmail.com>
2023-05-07 16:47:15 +0300

    save data from picker

--- app/src/main/java/com/vad/pomodoro/ui/PomodoroSettingsDialog.java
+++ app/src/main/java/com/vad/pomodoro/ui/PomodoroSettingsDialog.java
@@ -1,5 +1,6 @@
 package com.vad.pomodoro.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.vad.pomodoro.R;
+import com.vad.pomodoro.model.SaveConfiguration;
 
-public class PomodoroSettingsDialog extends DialogFragment {
+public class PomodoroSettingsDialog extends DialogFragment implements NumberPicker.OnValueChangeListener {
+
+    private SaveConfiguration configuration;
+    private NumberPicker numberPickerPomodoro;
+    private NumberPicker numberPickerShort;
+    private NumberPicker numberPickerLong;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        configuration = new SaveConfiguration(context);
+    }
 
     @Nullable
     @Override
@@ -24,22 +37,34 @@ public class PomodoroSettingsDialog extends DialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        NumberPicker numberPickerPomodoro = view.findViewById(R.id.numberPickerPomodoro);
+        numberPickerPomodoro = view.findViewById(R.id.numberPickerPomodoro);
         numberPickerPomodoro.setWrapSelectorWheel(false);
         numberPickerPomodoro.setMinValue(25);
         numberPickerPomodoro.setMaxValue(60);
+        numberPickerPomodoro.setOnValueChangedListener(this);
 
-        NumberPicker numberPickerShort = view.findViewById(R.id.numberPickerShort);
+        numberPickerShort = view.findViewById(R.id.numberPickerShort);
         numberPickerShort.setWrapSelectorWheel(false);
         numberPickerShort.setMinValue(5);
         numberPickerShort.setMaxValue(15);
+        numberPickerShort.setOnValueChangedListener(this);
 
-        NumberPicker numberPickerLong = view.findViewById(R.id.numberPickerLong);
+        numberPickerLong = view.findViewById(R.id.numberPickerLong);
         numberPickerLong.setWrapSelectorWheel(false);
         numberPickerLong.setMinValue(15);
         numberPickerLong.setMaxValue(30);
+        numberPickerLong.setOnValueChangedListener(this);
 
+    }
 
-
+    @Override
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+        if (picker.equals(numberPickerPomodoro)) {
+            configuration.savePomodoro(newVal);
+        } else if (picker.equals(numberPickerShort)) {
+            configuration.saveShort(newVal);
+        } else if (picker.equals(numberPickerLong)) {
+            configuration.saveLong(newVal);
+        }
     }
 }
